4463849aa7336ed8b7fd9203fc8f6eb93307867a
Dzmitry Kishylau <dzmitry_kishylau@Dzmitrys-MacBook-Pro.local>
2021-12-12 21:27:52 -0800

    Add a simple website to display popular URLs

---
+++ website/src/App.js
@@ -0,0 +1,240 @@
+import React from 'react';
+import './App.css';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import BTable from 'react-bootstrap/Table';
+
+import { initializeApp } from "firebase/app";
+import {
+  getFirestore,
+  getDoc,
+  doc
+} from 'firebase/firestore/lite';
+
+import { useSortBy, useTable } from 'react-table'
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCrp3c1DBj63oSJc1tos_pSkCCWCvpylLs",
+  authDomain: "web3twitterdata.firebaseapp.com",
+  projectId: "web3twitterdata",
+  storageBucket: "web3twitterdata.appspot.com",
+  messagingSenderId: "794734525030",
+  appId: "1:794734525030:web:0364e799559704951acba7"
+};
+
+const RANGE_OPTIONS = [
+  {
+    title: "Last Month",
+    value: "lastMonth",
+  },
+  {
+    title: "Last Week",
+    value: "lastWeek",
+  },
+  {
+    title: "Last 2 Days",
+    value: "last2days",
+  }
+];
+const ACCEPTABLE_RANGE_VALUES = RANGE_OPTIONS.map(option => option.value);
+
+// Initialize Firebase
+initializeApp(firebaseConfig);
+const db = getFirestore();
+
+/**
+ *
+ * @param {string} range - document key. See ACCEPTABLE_RANGE_VALUES for options.
+ * @returns array of structures describing popular urls and their stats
+ */
+async function getPopularUrls(range) {
+  if (!ACCEPTABLE_RANGE_VALUES.includes(range)) {
+    console.error("Invalid range value", range);
+    return [];
+  }
+  const docSnap = await getDoc(doc(db, "urlsData", range));
+
+  if (docSnap.exists()) {
+    return docSnap.data().urls;
+  } else {
+    console.error("The document doesn't exist", range);
+    return [];
+  }
+}
+
+/**
+ * Get the current value for date range from url.
+ * @returns one of the values from ACCEPTABLE_RANGE_VALUES
+ */
+function getRangeParameter() {
+  const windowUrl = window.location.search;
+  const params = new URLSearchParams(windowUrl);
+  const range = params.get("range");
+  if (!range || !ACCEPTABLE_RANGE_VALUES.includes(range)) {
+    return "lastMonth";
+  }
+  return range;
+}
+
+/**
+ * Simple React component that wraps children in <b></b> if condition is true.
+ */
+function ConditionalBold({condition, children}) {
+  if (condition) {
+    return (<b>{children}</b>);
+  } else {
+    return children;
+  }
+}
+
+/**
+ * React component to render date range selector.
+ * @param {string} selectedRange - currently selected date range, a value from ACCEPTABLE_RANGE_VALUES
+ */
+function RangeSelector({selectedRange}) {
+  const rangeLinks = RANGE_OPTIONS.map(option => {
+    return (
+      <span key={option.value}>
+        <a href={"?range=" + option.value}>
+          <ConditionalBold condition={option.value === selectedRange}>
+            {option.title}
+          </ConditionalBold>
+        </a>
+        &nbsp;
+      </span>
+    );
+});
+  return (
+    <div>
+      Date range: {rangeLinks}
+    </div>
+  );
+}
+
+/**
+ * React component that displays a table with popular urls and their stats.
+ *
+ * @param {struct[]} urls - array of structures describing popular urls and their statistics
+ * @returns
+ */
+function UrlsTable({urls}) {
+  // table column definitions
+  const columns = React.useMemo(() => [
+    {
+      Header: 'URL',
+      accessor: (originalRow, rowIndex) => ({
+        url: originalRow['mentioned_url'],
+        title: originalRow['title'],
+      }),
+      Cell: ({value}) => {
+        const {url, title} = value;
+        return (<a href={url}>{title}</a>);
+      },
+    },
+    {
+      Header: 'Mentions',
+      accessor: 'mentions_count',
+    },
+    {
+      Header: 'Mentions by influencers',
+      accessor: 'influencer_mentions_count',
+    },
+    {
+      Header: 'Quotes',
+      accessor: 'quote_count',
+    },
+    {
+      Header: 'Retweets',
+      accessor: 'retweet_count',
+    },
+    {
+      Header: 'Mentioned by',
+      accessor: 'mentioned_by_influencers',
+      Cell: ({value}) => {
+        const influencerProfiles = value.map(username => (<span key={username}><a href={"https://twitter.com/" + username}>@{username}</a>&nbsp;</span>));
+        return (<span>{influencerProfiles}</span>)
+      },
+    },
+  ], []);
+
+  const {
+    getTableProps,
+    getTableBodyProps,
+    headerGroups,
+    rows,
+    prepareRow,
+  } = useTable({
+    columns,
+    data: urls,
+  }, useSortBy);
+
+  return (
+    <BTable striped bordered hover size="sm" {...getTableProps()}>
+      <thead>
+        {headerGroups.map(headerGroup => (
+          <tr {...headerGroup.getHeaderGroupProps()}>
+            {headerGroup.headers.map(column => (
+              // Add the sorting props to control sorting. For this example
+                // we can add them into the header props
+                <th {...column.getHeaderProps(column.getSortByToggleProps())}>
+                  {column.render('Header')}
+                  {/* Add a sort direction indicator */}
+                  <span>
+                    {column.isSorted
+                      ? column.isSortedDesc
+                        ? ' 🔽'
+                        : ' 🔼'
+                      : ''}
+                  </span>
+                </th>
+            ))}
+          </tr>
+        ))}
+      </thead>
+      <tbody {...getTableBodyProps()}>
+        {rows.map((row, i) => {
+          prepareRow(row)
+          return (
+            <tr {...row.getRowProps()}>
+              {row.cells.map(cell => {
+                return <td {...cell.getCellProps()}>{cell.render('Cell')}</td>
+              })}
+            </tr>
+          )
+        })}
+      </tbody>
+    </BTable>
+  );
+}
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      urls: [],
+      range: getRangeParameter(),
+    }
+  }
+
+  componentDidMount() {
+    // Load popular urls
+    getPopularUrls(this.state.range).then((urls) => {
+      this.setState((state, props) => ({ urls }));
+    });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <br/>
+        <RangeSelector selectedRange={this.state.range} />
+        <br/>
+        <UrlsTable urls={this.state.urls} />
+      </div>
+    );
+  }
+}
+
+export default App;
